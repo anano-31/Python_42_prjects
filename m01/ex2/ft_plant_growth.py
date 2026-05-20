0dde763158da545b3e1ac367aa4d6533b0be4654
anano-31 <revazishviliianano@gmail.com>
2026-05-20 19:12:40 +0200

    new version

--- m01/ex2/ft_plant_growth.py
+++ m01/ex2/ft_plant_growth.py
@@ -1,29 +1,25 @@
-# === Day 1 ===
-# Rose: 25cm, 30 days old
-# === Day 7 ===
-# Rose: 31cm, 36 days old
-# Growth this week: +6cm
-
 class Plant:
     def __init__(self, name, height, age):
         self.name = name
         self.height = height
         self.age = age
-    def age(self):
+    def age_(self):
         self.age += 1
-    def height(self):
+    def growth(self):
         self.height +=1
     def get_info(self):
         print(f"{self.name}: {self.height}cm, {self.age} days old")
 
 if __name__ == "__main__":
     rose = Plant("Rose", 25, 30)
+    plants = [rose]
     print("=== Day 1 ===")
-    rose.get_info()
+    plants[0].get_info()
 
-    for day in range(1, 7):
-        rose.height
-        rose.age
+    for p in plants:
+        for day in range(1, 7):
+            p.growth()
+            p.age_()
     
     print("=== Day 7 ===")
     rose.get_info()
